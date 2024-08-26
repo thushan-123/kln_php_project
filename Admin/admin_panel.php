90abaf5853501bc9d8b1c34f89fe9b3ba23f6053
thushan-123 <thushanmadusanka456@gmail.com>
2024-08-26 10:01:35 +0530

    create a show catergories in admin

--- Admin/admin_panel.php
+++ Admin/admin_panel.php
@@ -101,4 +101,77 @@ if(cookie_checker()){
 
 echo "</div>";
 
+if (isset($_POST['delete_category'])){
+    try{
+
+        $category_id = (int) $_POST['category_id'];
+
+        $query = "DELETE FROM flowers_category WHERE category_id=$category_id";
+        $result = mysqli_query($connection, $query);
+        logger("INFO", "$category_id id delete successfully");
+
+    }catch(Exception $e){
+        logger('ERROR', $e->getMessage());
+    }
+}
+
+echo "<div id='flowers_category'>";
+    echo "<h4>Flowers Category</h4>";
+    echo "<div id='show_category'>";
+        // show the all category
+
+        if (cookie_checker()){
+            try{
+
+                $query = "SELECT * FROM flowers_category";
+
+                $result = mysqli_query($connection, $query);
+
+                logger("INFO", "get flowers_category data successfully");
+
+                // display the aviable category
+
+                if (mysqli_num_rows($result) > 0){
+
+                    echo "<table id='show_category_table' border='1'>
+
+                            <tr><th>Category</th>
+                            <th>Delete</th></tr>       
+                    ";
+                    while($row = mysqli_fetch_assoc($result)){
+                        $category_id = $row["category_id"];
+                        $category_name = $row["category_name"];
+
+                        echo "<tr><td>$category_name</td>
+                            <td>
+                            <form action='' method='post'>
+                            <input type='hidden' name='category_id' value='$category_id'>
+                            <button type='submit' name='delete_category'>Delete</button>
+                            </form>
+                            </td>
+                            </tr>
+                        ";
+                    }
+
+                    echo "</table>";
+
+                }else{
+                    echo "<h3>NO Categories in table<h3>";
+                }
+                
+            }catch(Exception $e){
+                logger("ERROR", $e->getMessage());
+            }
+        }
+    echo "</div>";
+
+    echo "<div id='add_category'>";
+    echo "</div>";
+
+echo "</div>";
+
+
+echo "<div id='upload_flowers'>";
+
+echo "</div>";
 ?>
--- Connection/script.sql
+++ Connection/script.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (user_id int(10) AUTO_INCREMENT NOT NULL,
                     Index idx_email(email),
                     Index idx_user_name(user_name));
 
-CREATE Table admin (admin_id INT(10) AUTO_INCREMENT,
+CREATE TABLE admin (admin_id INT(10) AUTO_INCREMENT,
                     admin_name VARCHAR(20) NOT NULL UNIQUE,
                     email VARCHAR(30) NOT NULL UNIQUE,
                     password VARCHAR(80) NOT NULL,
@@ -22,7 +22,7 @@ INSERT INTO admin (admin_name, email, password) VALUES ('thush','thushanmadhusan
 
 
 
-CREATE Table supliers(suplier_id INT(10) AUTO_INCREMENT,
+CREATE TABLE supliers(suplier_id INT(10) AUTO_INCREMENT,
                       suplier_username VARCHAR(20) NOT NULL,
                       email VARCHAR(30) NOT NULL UNIQUE,
                       password VARCHAR(80) NOT NULL,
@@ -31,3 +31,31 @@ CREATE Table supliers(suplier_id INT(10) AUTO_INCREMENT,
                       PRIMARY KEY (suplier_id),
                       INDEX idx_suplier_id(suplier_id),
                       INDEX idx_email(email));
+
+CREATE TABLE flowers_category(category_id INT(5) AUTO_INCREMENT,
+                              category_name VARCHAR(20) NOT NULL,
+                              PRIMARY KEY (category_id));
+
+CREATE TABLE flowers(flower_id VARCHAR(100),
+                     flower_name VARCHAR(20),
+                     quantity INT(5),
+                     category_id INT(5),
+                     PRIMARY KEY (flower_id),
+                     Foreign Key (category_id) REFERENCES flowers_category(category_id)
+                     INDEX idx_flower_name(flower_name));
+
+
+CREATE TABLE flowers_prices(flower_id VARCHAR(100) UNIQUE,
+                            price INT(5),
+                            today_dicount INT(3),
+                            loyalty_discount INT(3),
+                            price_off INT(5),
+                            Foreign Key (flower_id) REFERENCES flowers(flower_id),
+                            INDEX idx_flower_id(flower_id));
+
+CREATE TABLE images_links(flower_id VARCHAR(100) UNIQUE,
+                          image_name VARCHAR(100) UNIQUE,
+                          file_path VARCHAR,
+                          Foreign Key (flower_id) REFERENCES flowers(flower_id),
+                          INDEX idx_flower_id(flower_id),
+                          INDEX idx_image_name(image_name));
